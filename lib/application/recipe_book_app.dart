0604be6f1247ed740be14cc9b28882deb6157cea
konstuanotop <konstantin-1997@mail.ru>
2026-04-01 20:13:41 +0500

    init application

---
+++ lib/application/recipe_book_app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class RecipeBookApp extends StatelessWidget {
+  const RecipeBookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const MyHomePage(title: 'Recipe Book'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 61, 72, 161),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[const Text('Recipe Book')],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_book/application/recipe_book_app.dart';
+
+void main() {
+  runApp(const RecipeBookApp());
+}
